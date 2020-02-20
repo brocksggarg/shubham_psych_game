79acdd3461764baf3ba54b0f933234b734e9ae7d
Shubham <shubham.garg1@pearson.com>
2020-02-20 16:39:25 +0530

    message added

--- src/main/java/com/psych/game/GameApplication.java
+++ src/main/java/com/psych/game/GameApplication.java
@@ -2,6 +2,7 @@ package com.psych.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +26,8 @@ class Hello{
 		return msg;
 	}
 	@RequestMapping("/add/{msg}")
-	public String addMsg(String m) {
-		msg=m;
+	public String addMsg(@PathVariable String msg) {
+		this.msg=msg;
 		return "added";
 	}
 }
