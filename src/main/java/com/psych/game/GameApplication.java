1de92c00acbb3c287145c520ac821e2d52188ee1
Shubham <shubham.garg1@pearson.com>
2020-03-01 09:32:22 +0530

    add basic functionality

--- src/main/java/com/psych/game/GameApplication.java
+++ src/main/java/com/psych/game/GameApplication.java
@@ -1,10 +1,14 @@
 package com.psych.game;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.psych.game.model.C;
+import com.psych.game.repositories.CRepository;
 
 @SpringBootApplication
 public class GameApplication {
@@ -21,8 +25,17 @@ public class GameApplication {
 class Hello{
 	String msg="";
 	
+	@Autowired
+	CRepository cr;
+	
 	@RequestMapping("/hello")
 	public String hello() {
+		C c=new C();
+		c.setId(12);
+		c.setName("ss");
+		c.setAlais("s");
+		c.setEmail("sss");
+		cr.save(c);
 		return msg;
 	}
 	@RequestMapping("/add/{msg}")
---
+++ src/main/java/com/psych/game/controller/DevTestController.java
@@ -0,0 +1,128 @@
+package com.psych.game.controller;
+
+import com.psych.game.model.*;
+import com.psych.game.repositories.GameRepository;
+import com.psych.game.repositories.PlayerRepository;
+import com.psych.game.repositories.QuestionRepository;
+import com.psych.game.repositories.UserRepository;
+import org.apache.catalina.util.ErrorPageSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/dev-test")
+public class DevTestController {
+    @Autowired
+    private PlayerRepository playerRepository;
+    @Autowired
+    private QuestionRepository questionRepository;
+    @Autowired
+    private GameRepository gameRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/populate")
+    public String populateDB() {
+        for(Player player: playerRepository.findAll()) {
+            player.getGames().clear();
+            playerRepository.save(player);
+        }
+        gameRepository.deleteAll();
+        playerRepository.deleteAll();
+        questionRepository.deleteAll();
+
+        Player luffy = new Player.Builder()
+                .alias("Monkey D. Luffy")
+                .email("luffy@interviewbit.com")
+                .saltedHashedPassword("strawhat")
+                .build();
+        playerRepository.save(luffy);
+        Player robin = new Player.Builder()
+                .alias("Nico Robin")
+                .email("robin@interviewbit.com")
+                .saltedHashedPassword("poneglyph")
+                .build();
+        playerRepository.save(robin);
+
+        Game game = new Game();
+        game.setGameMode(GameMode.IS_THIS_A_FACT);
+        game.setLeader(luffy);
+        game.getPlayers().add(luffy);
+        gameRepository.save(game);
+
+        questionRepository.save(new Question(
+                "What is the most important Poneglyph",
+                "Rio Poneglyph",
+                GameMode.IS_THIS_A_FACT
+        ));
+
+        questionRepository.save(new Question(
+                "How far can Luffy stretch?",
+                "56 Gomu Gomus",
+                GameMode.IS_THIS_A_FACT
+        ));
+
+        return "populated";
+    }
+
+    @GetMapping("/questions")
+    public List<Question> getAllQuestions() {
+        return questionRepository.findAll();
+    }
+
+    @GetMapping("/question/{id}")
+    public Optional<Question> getQuestionById(@PathVariable(name = "id") Long id) {
+        return questionRepository.findById(id);
+    }
+
+    @GetMapping("/players")
+    public List<Player> getAllPlayers() {
+        return playerRepository.findAll();
+    }
+
+    @GetMapping("/player/{id}")
+    public Optional<Player> getPlayerById(@PathVariable(name = "id") Long id) {
+        return playerRepository.findById(id);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public Optional<User> getUserById(@PathVariable(name = "id") Long id) {
+        return userRepository.findById(id);
+    }
+
+    @GetMapping("/games")
+    public List<Game> getAllGames() {
+        return gameRepository.findAll();
+    }
+
+    @GetMapping("/game/{id}")
+    public Optional<Game> getGameById(@PathVariable(name = "id") Long id) {
+        return gameRepository.findById(id);
+    }
+
+    // Games
+    // Players
+    // Admins
+    // Questions
+    // Rounds
+    // ContentWriters
+}
+
+// localhost:8080/dev-test/
