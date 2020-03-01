0c2a8b71ef5e7a3a9324f16c4b26024479128869
Shubham <shubham.garg1@pearson.com>
2020-03-01 09:52:31 +0530

    removed unneccarry code

--- src/main/java/com/psych/game/GameApplication.java
+++ src/main/java/com/psych/game/GameApplication.java
@@ -1,14 +1,7 @@
 package com.psych.game;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.psych.game.model.C;
-import com.psych.game.repositories.CRepository;
 
 @SpringBootApplication
 public class GameApplication {
@@ -17,30 +10,4 @@ public class GameApplication {
 		SpringApplication.run(GameApplication.class, args);
 	}
 
-}
-
-
-
-@RestController
-class Hello{
-	String msg="";
-	
-	@Autowired
-	CRepository cr;
-	
-	@RequestMapping("/hello")
-	public String hello() {
-		C c=new C();
-		c.setId(12);
-		c.setName("ss");
-		c.setAlais("s");
-		c.setEmail("sss");
-		cr.save(c);
-		return msg;
-	}
-	@RequestMapping("/add/{msg}")
-	public String addMsg(@PathVariable String msg) {
-		this.msg=msg;
-		return "added";
-	}
 }
